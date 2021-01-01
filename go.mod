8e5208bdf626051d31c3156739b0c2d6b096552e
calebikhuohon <calebikhuohon@gmail.com>
2021-01-01 15:51:07 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/calebikhuohon/kafka-play
+
+go 1.15
+
+require github.com/Shopify/sarama v1.27.2 // indirect
---
+++ kafka_async_producer.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"time"
+
+	"github.com/Shopify/sarama"
+)
+
+//Message to be sent
+type Message struct {
+	Who string
+	TimeAsString string
+}
+
+func main() {
+	config := sarama.NewConfig()
+
+	config.Producer.RequiredAcks = sarama.WaitForAll
+
+	config.Producer.Retry.Max = 5
+
+	brokers := []string{"localhost:9092", "localhost:9093", "localhost:9094"}
+	sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
+
+	asyncProducer, err := sarama.NewAsyncProducer(brokers, config)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := asyncProducer.Close(); err != nil {
+			log.Fatal(err)
+		}
+	} ()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	exitProgram := make(chan struct{})
+
+	var nPublished, nErrors int
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+
+			body := Message{
+				Who:          "aProcess",
+				TimeAsString: strconv.Itoa(int(time.Now().Unix())),
+			}
+
+			payload, _ := json.Marshal(body)
+
+			msg := &sarama.ProducerMessage{
+				Topic: "currentTime",
+				Key:   sarama.StringEncoder("aKey"),
+				Value: sarama.ByteEncoder(payload),
+			}
+
+			select {
+			case asyncProducer.Input() <- msg:
+				nPublished++
+				fmt.Println("Produce message")
+			case err := <- asyncProducer.Errors():
+				nErrors++
+				fmt.Println("failed to produce message: ", err)
+			case <- signals:
+				exitProgram <- struct{}{}
+			}
+
+			log.Printf("Published: %d; Errors: %d\n", nPublished, nErrors)
+		}
+	}()
+
+	<- exitProgram
+}
